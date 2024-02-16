e78582a21103e55462391010c1334d379363b495
SimonNyvall <simon.k.nyvall@proton.me>
2024-02-16 20:40:01 +0100

    Update the printing
    Add color and nerd font icons

--- src/TablePrinter.cs
+++ src/TablePrinter.cs
@@ -6,19 +6,30 @@ public class Data
 {
     public static void PrintBranchTable(List<TableRow> branchTable)
     {
-        int minimumBranchNameWidth = 11;
+        int minimumBranchNameWidth = 13;
         var longestBranchName = Math.Max(minimumBranchNameWidth, branchTable.Max(x => x.BranchName.Length));
 
-        string branchHeader = "Branch Name".PadRight(longestBranchName);
+        string branchHeader = "Branch Name ".PadRight(longestBranchName);
         string underline = new string('-', longestBranchName);
 
-        Console.WriteLine($"Ahead  |  Behind  | {branchHeader} | Last commit");
-        Console.WriteLine($"------ | -------- | {underline} | -----------");
+        string[] headers = { " Ahead 󰜘", " Behind 󰜘", branchHeader, "Last commit " };
+
+        Console.WriteLine();
+
+        foreach (var header in headers)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write($"{header}");
+            Console.ResetColor();
+            Console.Write(" | ");
+        }
+
+        Console.WriteLine($"\n-------- | --------- | {underline} | --------------");
 
         foreach (var branch in branchTable)
         {
-            var aHead = branch.Ahead.ToString().PadRight(6);
-            var behind = branch.Behind.ToString().PadRight(8);
+            var aHead = branch.Ahead.ToString().PadRight(8);
+            var behind = branch.Behind.ToString().PadRight(9);
             var branchName = branch.BranchName.PadRight(longestBranchName);
 
             Console.WriteLine($"{aHead} | {behind} | {branchName} | {branch.LastCommit.Item1} {branch.LastCommit.Item2}");
