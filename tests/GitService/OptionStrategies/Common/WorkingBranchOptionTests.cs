ff1bdb6f68f6e9c402da44377199b73b46dbfcbb
SimonNyvall <simon.k.nyvall@proton.me>
2024-07-08 04:12:46 +0200

    feat: Add unit tests for WorkingBranchOption class

---
+++ tests/GitService/OptionStrategies/Common/WorkingBranchOptionTests.cs
@@ -0,0 +1,44 @@
+using Git.Base;
+using Git.Options;
+using Shared.TableData;
+using NSubstitute;
+
+namespace Tests.GitService;
+
+public class WorkingBranchOptionTests
+{
+    [Fact]
+    public void Execute_ShouldReturnWorkingBranch_WhenBranchesAreProvided()
+    {
+        var mockGitBase = Substitute.For<IGitBase>();
+        mockGitBase.GetWorkingBranch().Returns("main");
+
+        var branches = new List<GitBranch>
+        {
+            GitBranch.Default().SetBranch(new Branch("main", isWorkingBranch: false)),
+            GitBranch.Default().SetBranch(new Branch("feature", isWorkingBranch: false))
+        };
+
+        var workingBranchOption = new WorkingBranchOption(mockGitBase);
+
+        List<GitBranch> result = workingBranchOption.Execute(branches);
+
+        Assert.True(result[0].Branch.IsWorkingBranch);
+        Assert.False(result[1].Branch.IsWorkingBranch);
+    }
+
+    [Fact]
+    public void Execute_ShouldReturnEmptyList_WhenEmptyBranchListAreProvided()
+    {
+        var mockGitBase = Substitute.For<IGitBase>();
+        mockGitBase.GetWorkingBranch().Returns("main");
+
+        List<GitBranch> branches = [];
+
+        var workingBranchOption = new WorkingBranchOption(mockGitBase);
+
+        List<GitBranch> result = workingBranchOption.Execute(branches);
+
+        Assert.Empty(result);
+    }
+}
