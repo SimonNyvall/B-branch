e993bf3e3a47f76acc913d4bf1049cb5ccc37560
NeotericSh <simon.k.nyvall@proton.me>
2024-08-08 23:53:24 +0200

    fix: spacing error in full table

--- src/CLI/Output/PrintFullTable.cs
+++ src/CLI/Output/PrintFullTable.cs
@@ -26,7 +26,7 @@ public class PrintFullTable
 
     private static void PrintHeaders(int longestBranchName)
     {
-        string branchHeader = $" Branch Name  ".PadRight(longestBranchName + 1);
+        string branchHeader = " Branch Name  ".PadRight(longestBranchName + 1);
         string underline = new('-', longestBranchName + 1);
         string[] headers = [ " Ahead 󰜘 ", " Behind 󰜘 ", branchHeader, " Last commit  " ];
 
@@ -45,11 +45,11 @@ public class PrintFullTable
         }
     }
 
-    private static void PrintBranchRow(GitBranch branch, int longestBranchName)
+    private static void PrintBranchRow(GitBranch branch, int longestBranchNameLength)
     {
         var aHead = branch.AheadBehind.Ahead.ToString().PadRight(8);
         var behind = branch.AheadBehind.Behind.ToString().PadRight(9);
-        var branchName = branch.Branch.Name.PadRight(longestBranchName);
+        var branchName = branch.Branch.Name.PadRight(longestBranchNameLength);
         var lastCommitText = GetTimePrefix(branch.LastCommit);
 
         Console.Write($" {aHead} |  {behind} |  ");
@@ -91,7 +91,9 @@ public class PrintFullTable
             return $"{time} Today";
         }
 
-        string timeElapsed = days == 1 ? "Day" : "Days";
-        return $"{days} {timeElapsed} ago";
+        string timeElapsed = days == 1 ? "day" : "days";
+
+        int padLeft = 5 - days.ToString().Length;
+        return $"{days} {new string(' ', padLeft)}{timeElapsed} ago";
     }
 }
