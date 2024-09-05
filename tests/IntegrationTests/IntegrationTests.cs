6f00fdd5a8a7522f61dbd72f7762df1cbb145559
NeotericSh <simon.k.nyvall@proton.me>
2024-09-05 16:14:34 +0200

    feat(integration-test): add remote flag test

--- tests/IntegrationTests/IntegrationTests.cs
+++ tests/IntegrationTests/IntegrationTests.cs
@@ -218,6 +218,32 @@ public partial class IntegrationTest
         }
     }
 
+    [Fact]
+    public async Task IntegrationTest_ValidOutput_WithRemoteFlag()
+    {
+        using var process = ProcessHelper.GetDotnetProcess("--remote");
+        process.Start();
+
+        string output = await process.StandardOutput.ReadToEndAsync();
+        string error = await process.StandardError.ReadToEndAsync();
+
+        process.WaitForExit();
+
+        Assert.True(string.IsNullOrEmpty(error), error);
+
+        string[] lines = output.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        AssertHeader(lines);
+
+        foreach (string line in lines.Skip(2))
+        {
+            var (ahead, behind) = GetAheadBehindFromString(line);
+
+            Assert.True(ahead >= 0, "ahead was below 0.");
+            Assert.True(behind >= 0, "behind was below 0.");
+        }
+    }
+
     private static void AssertHeader(string[] headerLines)
     {
         Assert.True(headerLines.Length >= 2, "Header lines does not contain enought lines for header print.");
