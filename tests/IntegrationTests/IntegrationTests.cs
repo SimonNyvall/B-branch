6d810790189258c580550e90717ed06148a658b7
NeotericSh <simon.k.nyvall@proton.me>
2024-09-05 15:56:23 +0200

    fix(integration-test): fix contains flag test

--- tests/IntegrationTests/IntegrationTests.cs
+++ tests/IntegrationTests/IntegrationTests.cs
@@ -132,6 +132,36 @@ public partial class IntegrationTest
         Assert.Equal(branchNames, sortedBranchNames);
     }
 
+    [Fact]
+    public async Task IntegrationTest_ValidOutput_WithContainsFlag()
+    {
+        using var process = ProcessHelper.GetDotnetProcess("--contains main");
+        process.Start();
+
+        string output = await process.StandardOutput.ReadToEndAsync();
+        string error = await process.StandardError.ReadToEndAsync();
+
+        process.WaitForExit();
+
+        Assert.True(string.IsNullOrEmpty(error), error);
+
+        string[] lines = output.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        AssertHeader(lines);
+
+        foreach (string line in lines.Skip(2))
+        {
+            var (ahead, behind) = GetAheadBehindFromString(line);
+
+            Assert.True(ahead >= 0, "ahead was below 0.");
+            Assert.True(behind >= 0, "behind was below 0.");
+        }
+
+        string[] branchNames = lines.Skip(2).Select(l => l.Split('|')[2].Trim()).ToArray();
+
+        Assert.All(branchNames, b => Assert.Contains("main", b, StringComparison.OrdinalIgnoreCase));
+    }
+
     private static void AssertHeader(string[] headerLines)
     {
         Assert.True(headerLines.Length >= 2, "Header lines does not contain enought lines for header print.");
