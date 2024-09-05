3b95b52c8f660ade502fb11e1294a6a2106bd09a
NeotericSh <simon.k.nyvall@proton.me>
2024-09-05 15:47:14 +0200

    feat(integration-test): add sort flag test

--- tests/IntegrationTests/IntegrationTests.cs
+++ tests/IntegrationTests/IntegrationTests.cs
@@ -100,6 +100,38 @@ public partial class IntegrationTest
         }
     }
 
+    [Fact]
+    public async Task IntegragionTest_ValidOutput_WithSortFlag()
+    {
+        using var process = ProcessHelper.GetDotnetProcess("--sort name");
+        process.Start();
+
+        string output = await process.StandardOutput.ReadToEndAsync();
+        string error = await process.StandardError.ReadToEndAsync();
+
+        process.WaitForExit();
+
+        Assert.True(string.IsNullOrEmpty(error), error);
+
+        string[] lines = output.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        AssertHeader(lines);
+
+        foreach (string line in lines.Skip(2))
+        {
+            var (ahead, behind) = GetAheadBehindFromString(line);
+
+            Assert.True(ahead >= 0, "ahead was below 0.");
+            Assert.True(behind >= 0, "behind was below 0.");
+        }
+
+        string[] branchNames = lines.Skip(2).Select(l => l.Split('|')[2].Trim()).ToArray();
+
+        string[] sortedBranchNames = branchNames.OrderBy(b => b).ToArray();
+
+        Assert.Equal(branchNames, sortedBranchNames);
+    }
+
     private static void AssertHeader(string[] headerLines)
     {
         Assert.True(headerLines.Length >= 2, "Header lines does not contain enought lines for header print.");
