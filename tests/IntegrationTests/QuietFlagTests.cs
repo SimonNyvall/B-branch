783be80f2a39d7731ac8e40f45ed0c98e8801815
SimonNyvall <simon.k.nyvall@proton.me>
2024-11-03 12:22:11 +0100

    Add integration tests for valid output with quiet flag

---
+++ tests/IntegrationTests/QuietFlagTests.cs
@@ -0,0 +1,51 @@
+namespace Bbranch.IntegrationTests;
+
+public class QuietFlagTests
+{
+    [Fact]
+    public async Task IntegrationTest_ValidOutput_WithQuietFlag()
+    {
+        await IntegrationTest_ValidOutput_WithQuietShortFlag();
+        await IntegrationTest_ValidOutput_WithQuietLongFlag();
+    }
+
+    private static async Task IntegrationTest_ValidOutput_WithQuietShortFlag()
+    {
+        using var process = ProcessHelper.GetDotnetProcess("-q");
+        process.Start();
+
+        string output = await process.StandardOutput.ReadToEndAsync();
+        string error = await process.StandardError.ReadToEndAsync();
+
+        process.WaitForExit();
+
+        Assert.True(string.IsNullOrEmpty(error), error);
+
+        string[] lines = output.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        Assert.DoesNotContain("Ahead 󰜘", lines[0]);
+        Assert.DoesNotContain("Behind 󰜘", lines[0]);
+        Assert.DoesNotContain("Branch Name ", lines[0]);
+        Assert.DoesNotContain("Last commit ", lines[0]);
+    }
+
+    private static async Task IntegrationTest_ValidOutput_WithQuietLongFlag()
+    {
+        using var process = ProcessHelper.GetDotnetProcess("--quiet");
+        process.Start();
+
+        string output = await process.StandardOutput.ReadToEndAsync();
+        string error = await process.StandardError.ReadToEndAsync();
+
+        process.WaitForExit();
+
+        Assert.True(string.IsNullOrEmpty(error), error);
+
+        string[] lines = output.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        Assert.DoesNotContain("Ahead 󰜘", lines[0]);
+        Assert.DoesNotContain("Behind 󰜘", lines[0]);
+        Assert.DoesNotContain("Branch Name ", lines[0]);
+        Assert.DoesNotContain("Last commit ", lines[0]);
+    }
+}
