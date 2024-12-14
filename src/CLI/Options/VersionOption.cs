33aac460f3fc397bb79a80c4bc0c1fd7f9f3a63b
SimonNyvall <simon.k.nyvall@proton.me>
2024-12-14 14:08:42 +0100

    update version

--- src/CLI/Options/VersionOption.cs
+++ src/CLI/Options/VersionOption.cs
@@ -4,7 +4,7 @@ public class VersionOption
 {
     public static void Execute()
     {
-        Console.WriteLine("v1.1.0");
+        Console.WriteLine("v1.1.1");
         Environment.Exit(0);
     }
 }
