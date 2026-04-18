a780718bb8655588ed042e239c74823badd4d0dc
JustHateMe <82256130+JustH8Me@users.noreply.github.com>
2026-04-18 18:54:56 +0500

    feat: Regex support for --contains and --no-contains (#217)

--- src/GitService/OptionStrategies/Common/ContainsStrategies/NoContainsOption.cs
+++ src/GitService/OptionStrategies/Common/ContainsStrategies/NoContainsOption.cs
@@ -1,4 +1,5 @@
 using Bbranch.Shared.TableData;
+using System.Text.RegularExpressions;
 
 namespace Bbranch.GitService.OptionStrategies.Common.ContainsStrategies;
 
@@ -8,6 +9,19 @@ public sealed class NoContainsOption(string pattern) : IOption
     {
         string[] patterns = ContainsSplit.SplitArgument(pattern);
 
-        return [.. branches.Where(branch => patterns.All(pattern => !branch.Branch.Name.Contains(pattern)))];
+        return
+        [
+            .. branches.Where(branch => patterns.All(p =>
+            {
+                try
+                {
+                    return !Regex.IsMatch(branch.Branch.Name, p, RegexOptions.IgnoreCase);
+                }
+                catch
+                {
+                    return !branch.Branch.Name.Contains(p, StringComparison.OrdinalIgnoreCase);
+                }
+            }))
+        ];
     }
 }
