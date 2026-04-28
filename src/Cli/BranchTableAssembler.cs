36a2a2f007249cda89ef5d34223af5cb02d88969
Simon Nyvall <simon.k.nyvall@outlook.com>
2026-04-28 19:59:26 +0200

    test: add assembly tests

--- src/Cli/BranchTableAssembler.cs
+++ src/Cli/BranchTableAssembler.cs
@@ -14,22 +14,28 @@ namespace Bbranch.CLI;
 
 public static class BranchTableAssembler
 {
-    private static readonly IGitRepository GitBase = GitRepository.GetInstance();
+    private static IGitRepository _gitRepository = null!;
+    internal static CompositeOptionStrategy _optionStrategies = null!;
 
-    internal static HashSet<GitBranch> AssembleBranchTable(FlagCollection arguments)
+    internal static HashSet<GitBranch> AssembleBranchTable(
+        IGitRepository gitRepository,
+        FlagCollection arguments
+    )
     {
+        _gitRepository = gitRepository;
+
         List<IOption> options = CreateOptions(arguments);
-        CompositeOptionStrategy optionStrategies = new(options);
+        _optionStrategies = new(options);
 
-        AddLastCommitOption(optionStrategies);
-        AddWorkingBranchOption(optionStrategies);
-        AddDescriptionOption(optionStrategies);
-        AddTrackOption(arguments, optionStrategies);
-        AddSortOption(arguments, optionStrategies);
-        AddContainsOptions(arguments, optionStrategies);
-        AddPrintTopOption(arguments, optionStrategies);
+        AddLastCommitOption();
+        AddWorkingBranchOption();
+        AddDescriptionOption();
+        AddTrackOption(arguments);
+        AddSortOption(arguments);
+        AddContainsOptions(arguments);
+        AddPrintTopOption(arguments);
 
-        return optionStrategies.Execute([]);
+        return _optionStrategies.Execute([]);
     }
 
     private static List<IOption> CreateOptions(FlagCollection arguments)
@@ -38,7 +44,7 @@ public static class BranchTableAssembler
 
         if (arguments.Contains<AllFlag>())
         {
-            IOption allOption = new BranchAllOptions(GitBase);
+            IOption allOption = new BranchAllOptions(_gitRepository);
             options.Add(allOption);
 
             return options;
@@ -46,35 +52,32 @@ public static class BranchTableAssembler
 
         if (arguments.Contains<RemoteFlag>())
         {
-            IOption remoteOption = new BranchRemoteOptions(GitBase);
+            IOption remoteOption = new BranchRemoteOptions(_gitRepository);
             options.Add(remoteOption);
 
             return options;
         }
 
-        IOption localOption = new BranchLocalOptions(GitBase);
+        IOption localOption = new BranchLocalOptions(_gitRepository);
         options.Add(localOption);
 
         return options;
     }
 
-    private static void AddLastCommitOption(CompositeOptionStrategy optionStrategies)
+    private static void AddLastCommitOption()
     {
-        IOption lastCommitOption = new SetLastCommitOptions(GitBase);
-        optionStrategies.AddStrategyOption(lastCommitOption);
+        IOption lastCommitOption = new SetLastCommitOptions(_gitRepository);
+        _optionStrategies.AddStrategyOption(lastCommitOption);
     }
 
-    private static void AddContainsOptions(
-        FlagCollection arguments,
-        CompositeOptionStrategy optionStrategies
-    )
+    private static void AddContainsOptions(FlagCollection arguments)
     {
         if (arguments.Contains<ContainsFlag>(out var containsFlag))
         {
             var value = containsFlag.Value!.ToString();
 
             IOption containsOption = new ContainsOption(value);
-            optionStrategies.AddStrategyOption(containsOption);
+            _optionStrategies.AddStrategyOption(containsOption);
 
             return;
         }
@@ -84,39 +87,33 @@ public static class BranchTableAssembler
             var value = noContainsFlag.Value;
 
             IOption noContainsOption = new NoContainsOption(value.ToString());
-            optionStrategies.AddStrategyOption(noContainsOption);
+            _optionStrategies.AddStrategyOption(noContainsOption);
         }
     }
 
-    private static void AddTrackOption(
-        FlagCollection arguments,
-        CompositeOptionStrategy optionStrategies
-    )
+    private static void AddTrackOption(FlagCollection arguments)
     {
         if (arguments.Contains<TrackFlag>(out var trackFlag))
         {
             var value = trackFlag.Value.ToString();
 
-            IOption trackOption = new TrackAheadBehindOption(GitBase, value);
-            optionStrategies.AddStrategyOption(trackOption);
+            IOption trackOption = new TrackAheadBehindOption(_gitRepository, value);
+            _optionStrategies.AddStrategyOption(trackOption);
 
             return;
         }
 
-        IOption aheadBehindOption = new DefaultAheadBehindOption(GitBase);
-        optionStrategies.AddStrategyOption(aheadBehindOption);
+        IOption aheadBehindOption = new DefaultAheadBehindOption(_gitRepository);
+        _optionStrategies.AddStrategyOption(aheadBehindOption);
     }
 
-    private static void AddWorkingBranchOption(CompositeOptionStrategy optionStrategies)
+    private static void AddWorkingBranchOption()
     {
-        IOption workingBranchOption = new WorkingBranchOption(GitBase);
-        optionStrategies.AddStrategyOption(workingBranchOption);
+        IOption workingBranchOption = new WorkingBranchOption(_gitRepository);
+        _optionStrategies.AddStrategyOption(workingBranchOption);
     }
 
-    private static void AddSortOption(
-        FlagCollection arguments,
-        CompositeOptionStrategy optionStrategies
-    )
+    private static void AddSortOption(FlagCollection arguments)
     {
         IOption sortOption;
 
@@ -127,50 +124,47 @@ public static class BranchTableAssembler
             if (value == "name")
             {
                 sortOption = new SortByNameOptions();
-                optionStrategies.AddStrategyOption(sortOption);
+                _optionStrategies.AddStrategyOption(sortOption);
             }
             else if (value == "ahead")
             {
                 sortOption = new SortByAheadOptions();
-                optionStrategies.AddStrategyOption(sortOption);
+                _optionStrategies.AddStrategyOption(sortOption);
             }
             else if (value == "behind")
             {
                 sortOption = new SortByBehindOptions();
-                optionStrategies.AddStrategyOption(sortOption);
+                _optionStrategies.AddStrategyOption(sortOption);
             }
             else if (value == "date")
             {
                 sortOption = new SortByLastCommitOptions();
-                optionStrategies.AddStrategyOption(sortOption);
+                _optionStrategies.AddStrategyOption(sortOption);
             }
-
-            return;
+        }
+        else
+        {
+            sortOption = new SortByLastCommitOptions();
+            _optionStrategies.AddStrategyOption(sortOption);
         }
 
-        sortOption = new SortByLastCommitOptions();
-        optionStrategies.AddStrategyOption(sortOption);
-
         var detachedHeadSortOption = new SortDetachedHeadOption();
-        optionStrategies.AddStrategyOption(detachedHeadSortOption);
+        _optionStrategies.AddStrategyOption(detachedHeadSortOption);
     }
 
-    private static void AddDescriptionOption(CompositeOptionStrategy optionStrategies)
+    private static void AddDescriptionOption()
     {
         IOption descriptionOption = new DescriptionOption();
-        optionStrategies.AddStrategyOption(descriptionOption);
+        _optionStrategies.AddStrategyOption(descriptionOption);
     }
 
-    private static void AddPrintTopOption(
-        FlagCollection arguments,
-        CompositeOptionStrategy optionStrategies
-    )
+    private static void AddPrintTopOption(FlagCollection arguments)
     {
         if (!arguments.Contains<PrintTopFlag>(out var printTopFlag))
             return;
 
         var topValue = Convert.ToInt32(printTopFlag.Value.ToString());
         IOption printTopOption = new TopOption(topValue);
-        optionStrategies.AddStrategyOption(printTopOption);
+        _optionStrategies.AddStrategyOption(printTopOption);
     }
 }
